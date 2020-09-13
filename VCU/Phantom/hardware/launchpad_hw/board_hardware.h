9fb0cabd749536092d37a8820ef74d2a7f509145
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-09-13 00:24:13 -0700

    Enabled "STATE_PRINT" to print VCU state on terminal for debugging.

--- VCU/Phantom/hardware/launchpad_hw/board_hardware.h
+++ VCU/Phantom/hardware/launchpad_hw/board_hardware.h
@@ -74,7 +74,7 @@
  *                          DEBUG PRINTING DEFINES
  *********************************************************************************/
 #define TASK_PRINT  1
-#define STATE_PRINT 0
+#define STATE_PRINT 1
 #define APPS_PRINT  0 // if this is enabled, it hogs the whole cpu since the task it runs in is called every 10ms and is the highest priority. doesn't allow other tasks/interrupts to run
 #define BSE_PRINT   0 // if this is enabled, it hogs the whole cpu since the task it runs in is called every 10ms and is the highest priority. doesn't allow other tasks/interrupts to run
 
