ab323a802861db8277a5bacd0fbba5e4bf4ef5b7
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-11-17 15:57:20 -0800

    Added new faults to VCU data struct.
    Documentation of new Faults:
    https://docs.google.com/document/d/1R-fJ0VAyK6HpsrrwhxrWxydJqgsh1hNZr3dGRHAYEkk

--- VCU/Phantom/data_structures/vcu_data.h
+++ VCU/Phantom/data_structures/vcu_data.h
@@ -45,17 +45,64 @@ typedef struct analogOutputs
  *                  RTDS = 0 -> Ready To Driver is not Set
  */
 
+/*
+ *
+ *  New faults: Documentation: https://docs.google.com/document/d/1R-fJ0VAyK6HpsrrwhxrWxydJqgsh1hNZr3dGRHAYEkk/edit - jjkhan
+ *
+ */
+
 typedef struct digitalValues
 {
+
+
     uint8_t TSAL_ON;
     uint8_t RTDS;
-    uint8_t BMS_FAULT;
-    uint8_t IMD_FAULT;
+    uint8_t BMS_GPIO_FAULT; // Changed BMS_FAULT to BMS_GPIO_FAULT - jjkhan
+    //uint8_t IMD_FAULT; -> No more IMD, there are a bunch of IMD faults to check for  - jjkhan
     uint8_t BSPD_FAULT;
-    uint8_t BSE_FAULT;
-    uint8_t APPS_FAULT;
+    // uint8_t BSE_FAULT;  -> No more BSE, there is BSE_SEVERE_RANGE_FAULT
+    // uint8_t APPS_FAULT; -> No more APPS, there is APPS1 and APPS2 check  - jjkhan
 
-    // many other faults..
+
+    /* ++ New Faults*/
+
+    /* APPS/BSE Sensor - Faults */
+
+    uint8_t BSE_SEVERE_RANGE_FAULT;
+    uint8_t APPS1_SEVERE_RANGE_FAULT;
+    uint8_t APPS2_SEVERE_RANGE_FAULT;
+    uint8_t APPS_SEVERE_10DIFF_FAULT;
+    uint8_t BSE_APPS_MINOR_SIMULTANEOUS_FAULT;
+
+    /* HV Current Sensor & Voltage Sensor- Faults */
+
+    uint8_t HV_CURRENT_OUT_OF_RANGE;
+    uint8_t APPS_PROPORTION_ERROR;
+    uint8_t HV_VOLTAGE_OUT_OF_RANGE_FAULT;
+
+    /* CAN Error Messages. */
+
+    uint8_t CAN_ERROR_TYPE1; // Severe Error reported by CAN
+    uint8_t CAN_ERRO_TYPE2;  // Minor Errot Reported by CAN
+
+    /* LV Current Sensor & Voltage Sensor- Faults */
+    uint8_t LV_CURRENT_OUT_OF_RANGE;
+    uint8_t LV_VOLTAGE_OUT_OF_RANGE;
+
+    /* IMD Faults. */
+    uint8_t IMD_LOW_ISO_FAULT;
+    uint8_t IMD_SHORT_CIRCUIT_FAULT;
+    uint8_t IMD_DEVICE_ERR_FAULT;
+    uint8_t IMD_UNDEF_ERR;
+    uint8_t IMD_GARBAGE_DATA_FAULT;
+
+    /* TSAL Faults.*/
+    uint8_t TSAL_WELDED_AIRS_FAULT;
+
+    /* -- New Faults  */
+
+
+
 } digitalValues;
 
 typedef struct digitalOutputs
