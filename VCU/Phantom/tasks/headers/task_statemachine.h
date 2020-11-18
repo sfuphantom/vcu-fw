cba8b40d083f48d891a41b4f592b86d29b6f8f80
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-11-17 21:26:39 -0800

    Added MACROS of possible Fault locations.
    Fault locations: Shutdown Circuit, APPS, BSE, CAN Message, HV and LV
    Sensors

--- VCU/Phantom/tasks/headers/task_statemachine.h
+++ VCU/Phantom/tasks/headers/task_statemachine.h
@@ -9,6 +9,33 @@
 #define PHANTOM_TASKS_TASK_STATEMACHINE_H_
 
 
+#define NOFAULT         0U
+#define FAULT           1U
+
+/*
+ * Fault - Bit Number
+ *      Can have 32 faults
+ *
+ *
+ */
+#define SDC_FAULT       1U
+#define BSE_APPS_FAULT  2U
+#define HV_LV_FAULT     3U
+#define CAN_FAULT       4U
+
+/*
+
+/* SET/RESET */
+#define SET         1U
+#define ON          SET
+
+
+
+/*
+ *  SDC Fault types
+ */
+#define
+
 void vStateMachineTask(void *);
 
 
