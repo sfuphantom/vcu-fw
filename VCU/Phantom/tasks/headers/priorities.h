65ad02f78c53763675987c75bb9c3f42cc58e4fe
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2021-03-11 22:18:11 -0800

    Created macros to define each task's period.

--- VCU/Phantom/tasks/headers/priorities.h
+++ VCU/Phantom/tasks/headers/priorities.h
@@ -8,17 +8,27 @@
 #ifndef TASKS_PRIORITIES_H_
 #define TASKS_PRIORITIES_H_
 
+//++ Added by jjkhan - For readability - use these MACROS with pdMS_TO_TICKS(MillisecondTime)
 
+#define THROTTLE_TASK_PERIOD_MS         pdMS_TO_TICKS(150)
+#define SENSOR_READ_TASK_PERIOD_MS      pdMS_TO_TICKS(80)
+#define STATE_MACHINE_TASK_PERIOD_MS    pdMS_TO_TICKS(50)
+#define EEPROM_TASK_PERIOD_MS           pdMS_TO_TICKS(25)
+#define DATA_LOGGING_TASK_PERIOD_MS     pdMS_TO_TICKS(10)
+#define WATCHDOG_TASK__PERIOD_MS        pdMS_TO_TICKS(10)
+
+// -- Added by jjkhan
 /*********************************************************************************
  *                              TASK PRIORITIES
  *********************************************************************************/
 
-#define THROTTLE_TASK_PRIORITY         3
-#define SENSOR_READ_TASK_PRIORITY      2
-#define STATE_MACHINE_TASK_PRIORITY    1
+#define THROTTLE_TASK_PRIORITY         4
+#define SENSOR_READ_TASK_PRIORITY      3
+#define STATE_MACHINE_TASK_PRIORITY    2
+#define EEPROM_TASK_PRIORITY           1
 #define DATA_LOGGING_TASK_PRIORITY     0    // same as idle task
 #define WATCHDOG_TASK_PRIORITY         0    // same as idle task
-#define EEPROM_TASK_PRIORITY           0
+
 
 // there may also be interrupt/ISR priorities for:
 // CAN messages
