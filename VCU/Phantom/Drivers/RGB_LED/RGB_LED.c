1fa96ad77dc430d8efd8cdb7943cab2cca81a022
Guojiaxi <joshliguo@gmail.com>
2021-05-20 17:45:23 -0700

    added RGB_LED implementation

--- VCU/Phantom/Drivers/RGB_LED/RGB_LED.c
+++ VCU/Phantom/Drivers/RGB_LED/RGB_LED.c
@@ -1,22 +1,66 @@
 /*
  * RGB_LED.c
  *
- *  Created on: Nov 19, 2020
- *      Author: gabriel
+ *  Created on: Mar 3, 2021
+ *      Authors: Joshua Li Guo
  */
+#include "gio.h"
+#include "het.h"
+#include "board_hardware.h"
 
 #include "RGB_LED.h"
 
+#if !defined(RGB_LED_PORT) || !defined(RED_LED) || !defined(GREEN_LED) || !defined(BLUE_LED)
 
-void RGB_LED_Init(void)
+#define RGB_LED_PORT          hetRAM1
+#define BLUE_LED              pwm1
+#define GREEN_LED             pwm2
+#define RED_LED               pwm3
+
+#endif
+
+#define DEFAULT_PWM_PERIOD_US   10.0
+#define DEFAULT_SIGNAL          (hetSIGNAL_t) {DEFAULT_PWM_DUTY, DEFAULT_PWM_PERIOD_US}
+
+// This function must be called before using
+// anything else in the module.
+void RGB_LED_Init()
 {
-    // turn off RGB LEDs
-    pwmStart(RGB_LED_PORT, BLUE_LED); // blue
-    pwmStart(RGB_LED_PORT, GREEN_LED); // green
-    pwmStart(RGB_LED_PORT, RED_LED); // red
-//     maybe this can be changed in halcogen?
+    pwmSetSignal(RGB_LED_PORT, RED_LED, DEFAULT_SIGNAL);
+    pwmSetSignal(RGB_LED_PORT, GREEN_LED, DEFAULT_SIGNAL);
+    pwmSetSignal(RGB_LED_PORT, BLUE_LED, DEFAULT_SIGNAL);
 
-//     initialize HET pins ALL to output.. may need to change this later
-    gioSetDirection(hetPORT1, 0xFFFFFFFF);
+    pwmStart(RGB_LED_PORT, RED_LED);
+    pwmStart(RGB_LED_PORT, GREEN_LED);
+    pwmStart(RGB_LED_PORT, BLUE_LED);
+
+    RGB_LED_drive(DEFAULT_COLOR, DEFAULT_PWM_DUTY);
 }
 
+void RGB_LED_reset()
+{
+    ASSERT(isInitialized);
+    
+    pwmStop(RGB_LED_PORT, RED_LED);
+    pwmStop(RGB_LED_PORT, GREEN_LED);
+    pwmStop(RGB_LED_PORT, BLUE_LED);
+
+    pwmSetSignal(RGB_LED_PORT, RED_LED, DEFAULT_SIGNAL);
+    pwmSetSignal(RGB_LED_PORT, GREEN_LED, DEFAULT_SIGNAL);
+    pwmSetSignal(RGB_LED_PORT, BLUE_LED, DEFAULT_SIGNAL);
+
+    pwmStart(RGB_LED_PORT, RED_LED);
+    pwmStart(RGB_LED_PORT, GREEN_LED);
+    pwmStart(RGB_LED_PORT, BLUE_LED);
+
+    RGB_LED_drive(DEFAULT_COLOR, DEFAULT_PWM_DUTY);
+}
+
+void RGB_LED_drive(uint8_t colorCode, uint32_t pwm)
+{
+    ASSERT(isInitialized);
+
+    pwmSetDuty(RGB_LED_PORT, RED_LED, pwm * ((colorCode & RGB_RED) && 1) + 1);
+    pwmSetDuty(RGB_LED_PORT, GREEN_LED, pwm * ((colorCode & RGB_GREEN) && 1) + 1);
+    pwmSetDuty(RGB_LED_PORT, BLUE_LED, pwm * ((colorCode & RGB_BLUE) && 1) + 1);
+}
--- VCU/Phantom/Drivers/RGB_LED/RGB_LED.h
+++ VCU/Phantom/Drivers/RGB_LED/RGB_LED.h
@@ -1,18 +1,49 @@
 /*
  * RGB_LED.h
  *
- *  Created on: Nov 19, 2020
- *      Author: gabriel
+ *  Created on: Mar 3, 2021
+ *      Authors: Rafael Guevara, Joshua Li Guo
  */
 
-#ifndef PHANTOM_DRIVERS_RGB_LED_RGB_LED_H_
-#define PHANTOM_DRIVERS_RGB_LED_RGB_LED_H_
+//Halcogen Setup
+//1.Enable HET Drivers
+//2.Enable pwm channel outputting to specified pin
+//3.Enable Output of pin in HET window
 
-#include "gio.h"
-#include "het.h"
-#include "board_hardware.h"
+#ifndef SOURCE_RGB_H_
+#define SOURCE_RGB_H_
 
-void RGB_LED_Init(void);
+#include <stdint.h>
 
+/*
+ * MACHINE STATE COLOR CODING
+ *
+ * TRACTIVE_OFF     CYAN
+ * TRACTIVE_ON      MAGENTA
+ * RUNNING          GREEN
+ * MINOR_FAULT      YELLOW
+ * SEVERE_FAULT     RED
+ */
 
-#endif /* PHANTOM_DRIVERS_RGB_LED_RGB_LED_H_ */
+#define RGB_OFF     0x00
+#define RGB_RED     0x01
+#define RGB_GREEN   0x02
+#define RGB_BLUE    0x04
+#define RGB_YELLOW  0x03
+#define RGB_MAGENTA 0x05
+#define RGB_CYAN    0x06
+#define RGB_WHITE   0x07
+
+#define DEFAULT_PWM_DUTY    25
+#define DEFAULT_COLOR       RGB_WHITE
+
+// Initialize the driver
+void RGB_LED_Init();
+
+// Reset driver to initial state
+void RGB_LED_reset();
+
+// colorCode is any of the RGB color macros above
+void RGB_LED_drive(uint8_t colorCode, uint32_t pwm);
+
+#endif /* SOURCE_RGB_H_ */
