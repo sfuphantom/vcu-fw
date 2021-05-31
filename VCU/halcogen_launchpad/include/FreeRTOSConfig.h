09f3797b36dda8d00a620f2a3bd479cfd2039291
rafguevara14 <[rafaelg@sfu.ca]>
2021-05-30 22:23:26 -0700

    raise priority of timer task to 3

--- VCU/halcogen_launchpad/include/FreeRTOSConfig.h
+++ VCU/halcogen_launchpad/include/FreeRTOSConfig.h
@@ -132,7 +132,7 @@
 
 /* Timers */
 #define configUSE_TIMERS                1
-#define configTIMER_TASK_PRIORITY		( 0 )
+#define configTIMER_TASK_PRIORITY		( 3 )
 #define configTIMER_QUEUE_LENGTH		4
 #define configTIMER_TASK_STACK_DEPTH	( 256 )
 
