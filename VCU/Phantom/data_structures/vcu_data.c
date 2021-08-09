a2c329b27e64aadc488932b842a2cc98504e636a
Guojiaxi <joshliguo@gmail.com>
2021-08-09 03:37:33 -0700

    mostly refactored vcu data module (still need to add prototypes to .h file)

--- VCU/Phantom/data_structures/vcu_data.c
+++ VCU/Phantom/data_structures/vcu_data.c
@@ -4,68 +4,173 @@
  *  Created on: Apr 18, 2020
  *      Author: soroush
  */
+#include "hal_stdtypes.h"
+#include "FreeRTOS.h"
+#include "os_semphr.h"
+
 #include "vcu_data.h"
 
+static analogData analogDataArray[_ANALOG_DATA_LENGTH];
+
+/* DIGITAL DATA */
+static bool RTD_signal;
+static bool brake_light_signal; // originally named BRAKE_LIGHT_ENABLED
+
+static uint8 fault_flags; // fault_flags can hold 8 different faults
+
+static State VCU_state;
+
+#define MUTEX_POLLING_TIME_MS   10
+static SemaphoreHandle_t VCU_Key = NULL;
+
+/*
+typedef struct data
+{
+    Note: -> DO NOT change the place of vcuState structure in this file because:
+         *  In eepromTask, we're reading vcuState from FEE bank using an OFFSET of 72 bytes.
+         *  i.e. 73rd byte is the value corresponding to vcuState, based on the setup on
+         *  the placement order of "data" structure below.
+
+         *  Where did this 72 bytes number come from? Also, what is it offsetting from?
+
+    Note: Wow, this method needs to change. Reading from a raw memory position, while it may work
+        if and only if the position is correct, is a terribly dangerous and unstable way to read
+        the data you need. This method also completely restricts where and what the data structure
+        can be and can hold without the headache of actually having to find where in physical memory it is.
+
+} data;
+*/
 
 /*******************************************************************
-* NAME :            initData
-*
-* DESCRIPTION :     Initializes the VCU State Machine
-*
-* INPUTS : Pointer to baseAddress to data structure of VCU data.
-*
-* RETURN : None
-*
-*
-* NOTES :
-*
+* NOTES : This function must be called before any operation on the VCU
+*         data structure. Otherwise, undefined behaviour will happen.
 */
-void initData(data* VCUDataPtr)
+void VCUData_init(void)
 {
-    /***********************************************************
-     *              ANALOG VALUE INITIALIZATION
-     ***********************************************************/
+    VCU_Key = xSemaphoreCreateMutex();
 
-    VCUDataPtr->AnalogIn.APPS1_percentage.adc_value = 0;
-    VCUDataPtr->AnalogIn.APPS1_percentage.value = 0.0;
+    for (int i = 0; i < _ANALOG_DATA_LENGTH; i++) {
+        analogDataArray[i] = (analogData) {0.0, 0.0};
+    }
 
-    VCUDataPtr->AnalogIn.APPS2_percentage.adc_value = 0;
-    VCUDataPtr->AnalogIn.APPS2_percentage.value = 0.0;
-
-    VCUDataPtr->AnalogIn.BSE_percentage.adc_value = 0;
-    VCUDataPtr->AnalogIn.BSE_percentage.value = 0.0;
-
-    VCUDataPtr->AnalogIn.currentHV_A.adc_value = 0;
-    VCUDataPtr->AnalogIn.currentHV_A.value = 0.0;
-
-    VCUDataPtr->AnalogIn.voltageHV_V.adc_value = 0;
-    VCUDataPtr->AnalogIn.voltageHV_V.value = 0.0;
-
-    VCUDataPtr->AnalogIn.currentLV_A.adc_value = 0;
-    VCUDataPtr->AnalogIn.currentLV_A.value = 0.0;
-
-    VCUDataPtr->AnalogIn.voltageLV_V.adc_value = 0;
-    VCUDataPtr->AnalogIn.voltageLV_V.value = 0.0;
-
-    VCUDataPtr->AnalogOut.throttle_percentage.adc_value = 0;
-    VCUDataPtr->AnalogOut.throttle_percentage.value = 0.0;
-
-    /***********************************************************
-     *              DIGITAL VALUE INITIALIZATION
-     ***********************************************************/
-
-    VCUDataPtr->DigitalOut.BRAKE_LIGHT_ENABLE = 0;
-
-    VCUDataPtr->DigitalVal.RTDS = 1;
-    VCUDataPtr->DigitalVal.BMS_FAULT = 1;
-    VCUDataPtr->DigitalVal.IMD_FAULT = 1;
-    VCUDataPtr->DigitalVal.BSPD_FAULT = 1;
-
-    VCUDataPtr->DigitalVal.TSAL_FAULT = 1;
-    VCUDataPtr->DigitalVal.BSE_FAULT = 0;
-    VCUDataPtr->DigitalVal.APPS_FAULT = 0;
-    // ++ Added by jjkhan
-    VCUDataPtr->vcuState = TRACTIVE_OFF;
-    // -- Added by jjkhan
+    signal_flags = RTD_SIGNAL_MASK;
+    fault_flags = 0U;
+    VCU_state = TRACTIVE_OFF;
 }
 
+
+/* ANALOG DATA FUNCTIONS */
+analogData VCU_getAnalogData(AnalogValueIndex keyword)
+{
+    return analogDataArray[keyword];
+}
+
+bool VCU_setAnalogData(AnalogValueIndex keyword, analogData newData)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        analogDataArray[keyword] = newData;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+
+/* SIGNAL FUNCTIONS */
+bool VCUData_getRTDSignal(void)
+{
+    return RTD_signal;
+}
+
+bool VCUData_setRTDSignal(bool newSignal)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        RTD_signal = newSignal;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+bool VCUData_getBrakeLightSignal(void)
+{
+    return brake_light_signal;
+}
+
+bool VCUData_setBrakeLightSignal(bool newSignal)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        brake_light_signal = newSignal;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+
+/* FAULT FUNCTIONS */
+uint8 VCUData_readFaults(uint8 mask)
+{
+    return fault_flags & mask;
+}
+
+bool VCUData_turnOnFaults(uint8 mask)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        fault_flags |= mask;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+bool VCUData_turnOffFaults(uint8 mask)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        fault_flags &= ~mask;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+bool VCUData_setFaults(uint8 newFaultBitSet)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        fault_flags = newFaultBitSet;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
+
+
+/* STATE FUNCTIONS */
+State VCUData_getState(void)
+{
+    return VCU_state;
+}
+
+bool VCUData_setState(State newState)
+{
+    if (xSemaphoreTake(VCU_Key, pdMS_TO_TICKS(MUTEX_POLLING_TIME_MS))) {
+
+        VCU_state = newState;
+
+        return xSemaphoreGive(VCU_Key);
+    } else {
+        return false;
+    }
+}
--- VCU/Phantom/data_structures/vcu_data.h
+++ VCU/Phantom/data_structures/vcu_data.h
@@ -2,85 +2,53 @@
  * vcu_data.h
  *
  *  Created on: Apr 18, 2020
- *      Author: soroush
+ *      Author: soroush, Josh Guo
  */
 
 #ifndef PHANTOM_DATA_STRUCTURES_VCU_DATA_H_
 #define PHANTOM_DATA_STRUCTURES_VCU_DATA_H_
-
-
-#include "sys_common.h"
-
-
-//++ Added by jjkhan
-#include "board_hardware.h"  // Has "State" enum definition
-//-- Added by jjkhan
-
-typedef struct analogData
-{
+typedef struct {
     float adc_value;
     float value; // in the unit of measurement
 } analogData;
 
-typedef struct analogInputs
-{
-    analogData currentHV_A;
-    analogData voltageHV_V;
-    analogData currentLV_A;
-    analogData voltageLV_V;
-    analogData BSE_percentage;
-    analogData APPS1_percentage;
-    analogData APPS2_percentage;
-} analogInputs;
+typedef enum {
+    currentHV_A,
+    voltageHV_V,
+    currentLV_A,
+    voltageLV_V,
+    BSE_percentage,
+    APPS1_percentage,
+    APPS2_percentage,
+    throttle_percentage,
 
-typedef struct analogOutputs
-{
-    analogData throttle_percentage;
-} analogOutputs;
+    // leave this as the last item in enum
+    _ANALOG_DATA_LENGTH
+} AnalogValueIndex;
 
-typedef struct digitalValues
-{
-    uint8_t RTDS;               // if 1, ready to drive is set, if 0 then not ready to drive
-    uint8_t BMS_FAULT;         // if 1, then BMS is healthy, if 0 then BMS fault
-    uint8_t IMD_FAULT;         // if 1, then IMD is healthy, if 0 then IMD fault
-    uint8_t BSPD_FAULT;        // if 1, then BSPD is healthy, if 0 then BSPD fault
+typedef enum {
+    BMS_FAULT_MASK = 1U,      // if 0, then BMS is healthy, if 1 then BMS fault
+    IMD_FAULT_MASK = 2U,      // if 0, then IMD is healthy, if 1 then IMD fault
+    BSPD_FAULT_MASK = 4U,      // if 0, then BSPD is healthy, if 1 then BSPD fault
+    TSAL_FAULT_MASK = 8U,      // if 0, then TSAL is on and AIRs are closed, if 1 then TSAL is off and AIRs should be open
+    BSE_FAULT_MASK = 16U,     // if 0, then BSE is healthy, if 1 then BSE fault
+    APPS_FAULT_MASK = 32U,     // if 0, then APPS is healthy, if 1 then APPS fault
+    ALL_FAULTS_MASK = (~0U)   // All 1s to let all faults through the mask
+} FaultMasks;
 
-    uint8_t TSAL_FAULT;        // if 1, then TSAL is on and AIRs are closed, if 0 then TSAL is off and AIRs should be open
-    uint8_t BSE_FAULT;          // if 0, then BSE is healthy, if 1 then BSE fault
-    uint8_t APPS_FAULT;         // if 0, then APPS is healthy, if 1 then APPS fault
-
-    // many other faults..
-} digitalValues;
-
-typedef struct digitalOutputs
-{
-    uint8_t BRAKE_LIGHT_ENABLE; // if 1, then BSE is pressed and brake light should turn on, if 0 then brake light is off
-    // somehow show the PWM for the buzzer here
-} digitalOutputs;
-
-typedef struct data
-{
-    /* Note: -> DO NOT change the place of vcuState structure in this file because:
-         *  In eepromTask, we're reading vcuState from FEE bank using an OFFSET of 72 bytes.
-         *  i.e. 73rd byte is the value corresponding to vcuState, based on the setup on
-         *  the placement order of "data" structure below.
-     */
-
-    analogInputs AnalogIn;
-    analogOutputs AnalogOut;
-    digitalOutputs DigitalOut;
-    digitalValues DigitalVal;
-
-    //++ Added by jjkhan
-    State vcuState;
-    //-- Added by jjkhan
-
-} data;
-
+typedef enum {
+    TRACTIVE_OFF, 
+    TRACTIVE_ON, 
+    RUNNING, 
+    MINOR_FAULT,
+    SEVERE_FAULT
+} State;
 
 
 // function prototypes
 
-void initData(data* VCUDataPtr);
+void VCUData_init(data* VCUDataPtr);
+
+
 
 #endif /* PHANTOM_DATA_STRUCTURES_VCU_DATA_H_ */
