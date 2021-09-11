b3d84ab5e2273ee618f6adfbd0762c537bef162d
jaypacamarra <jpacamar97@gmail.com>
2021-09-11 14:26:14 -0700

    Josh's changes on jay's computer

--- VCU/Phantom/tasks/source/task_test.c
+++ VCU/Phantom/tasks/source/task_test.c
@@ -21,7 +21,7 @@ static void testTimerCallback(TimerHandle_t timer);
 static Task testTask = {vTestTask, TEST_TASK_PERIOD_MS};
 // static TaskHandle_t taskHandle;
 
-// static TimerHandle_t testTimer;
+static TimerHandle_t testTimer;
 static long testTimerCounter = 0;
 
 void testTaskInit(void)
@@ -30,21 +30,12 @@ void testTaskInit(void)
     /*taskHandle = */Phantom_createTask(&testTask, "TestTask", TEST_TASK_STACK_SIZE, TEST_TASK_PRIORITY);
 
     // create recurring timers here...
-    /*testTimer = */Phantom_createTimer("testTimer", 1000, true, &testTimerCounter, testTimerCallback);
+    testTimer = Phantom_createTimer("testTimer", 1000, true, &testTimerCounter, testTimerCallback);
+    Phantom_startTimer(testTimer);
 
     // so compiler won't complain about unused variables
     // taskHandle;
     // testTimer;
-}
-
-static void testTimerCallback(TimerHandle_t timer)
-{
-    uint8 faults = VCUData_readFaults(ALL_FAULTS_MASK);
-    if (!faults) {  // prefer to avoid anything with mutexes in a timer callback
-        VCUData_turnOnFaults(BMS_FAULT_MASK | BSPD_FAULT_MASK | BSE_FAULT_MASK);
-    } else {
-        VCUData_turnOffFaults(BMS_FAULT_MASK | BSPD_FAULT_MASK | BSE_FAULT_MASK);
-    }
 }
 
 static void vTestTask(void* arg)
@@ -55,5 +46,15 @@ static void vTestTask(void* arg)
         RGB_LED_drive(RGB_YELLOW);
     } else {
         RGB_LED_drive(RGB_OFF);
+    }
+}
+
+static void testTimerCallback(TimerHandle_t timer)
+{
+    uint8 faults = VCUData_readFaults(ALL_FAULTS_MASK);
+    if (!faults) {  // prefer to avoid anything with mutexes in a timer callback
+        VCUData_turnOnFaults(BMS_FAULT_MASK | BSPD_FAULT_MASK | BSE_FAULT_MASK);
+    } else {
+        VCUData_turnOffFaults(BMS_FAULT_MASK | BSPD_FAULT_MASK | BSE_FAULT_MASK);
     }
 }
