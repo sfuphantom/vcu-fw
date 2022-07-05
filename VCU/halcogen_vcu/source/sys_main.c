4fb1009f5eaee6a9df67764ce92cc2c4326df790
rafguevara14 <66582348+rafguevara14@users.noreply.github.com>
2022-07-04 21:14:23 -0700

    new halcogen generation

--- VCU/halcogen_vcu/source/sys_main.c
+++ VCU/halcogen_vcu/source/sys_main.c
@@ -1,55 +1,55 @@
-// /** @file sys_main.c 
-// *   @brief Application main file
-// *   @date 11-Dec-2018
-// *   @version 04.07.01
-// *
-// *   This file contains an empty main function,
-// *   which can be used for the application.
-// */
+/** @file sys_main.c 
+*   @brief Application main file
+*   @date 11-Dec-2018
+*   @version 04.07.01
+*
+*   This file contains an empty main function,
+*   which can be used for the application.
+*/
 
-// /* 
-// * Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
-// * 
-// * 
-// *  Redistribution and use in source and binary forms, with or without 
-// *  modification, are permitted provided that the following conditions 
-// *  are met:
-// *
-// *    Redistributions of source code must retain the above copyright 
-// *    notice, this list of conditions and the following disclaimer.
-// *
-// *    Redistributions in binary form must reproduce the above copyright
-// *    notice, this list of conditions and the following disclaimer in the 
-// *    documentation and/or other materials provided with the   
-// *    distribution.
-// *
-// *    Neither the name of Texas Instruments Incorporated nor the names of
-// *    its contributors may be used to endorse or promote products derived
-// *    from this software without specific prior written permission.
-// *
-// *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-// *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-// *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-// *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-// *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-// *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// *
-// */
+/* 
+* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
+* 
+* 
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions 
+*  are met:
+*
+*    Redistributions of source code must retain the above copyright 
+*    notice, this list of conditions and the following disclaimer.
+*
+*    Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the   
+*    distribution.
+*
+*    Neither the name of Texas Instruments Incorporated nor the names of
+*    its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
 
 
-// /* USER CODE BEGIN (0) */
+/* USER CODE BEGIN (0) */
 // /* USER CODE END */
 
-// /* Include Files */
+/* Include Files */
 
-// #include "sys_common.h"
+#include "sys_common.h"
 
-// /* USER CODE BEGIN (1) */
+/* USER CODE BEGIN (1) */
 // #include "system.h" // is this required?
 
 // #include "FreeRTOS.h"
@@ -74,15 +74,15 @@
 
 // /* USER CODE END */
 
-// /** @fn void main(void)
-// *   @brief Application main function
-// *   @note This function is empty by default.
-// *
-// *   This function is called after startup.
-// *   The user can use this function to implement the application.
-// */
+/** @fn void main(void)
+*   @brief Application main function
+*   @note This function is empty by default.
+*
+*   This function is called after startup.
+*   The user can use this function to implement the application.
+*/
 
-// /* USER CODE BEGIN (2) */
+/* USER CODE BEGIN (2) */
 // /*********************************************************************************
 //  *                          DEBUG PRINTING DEFINES
 //  *********************************************************************************/
@@ -173,9 +173,9 @@
 // int lv_current = 0;
 // /* USER CODE END */
 
-// int main(void)
-// {
-// /* USER CODE BEGIN (3) */
+int main(void)
+{
+/* USER CODE BEGIN (3) */
 // /*********************************************************************************
 //  *                          HALCOGEN PERIPHERAL INITIALIZATION
 //  *********************************************************************************/
@@ -349,11 +349,11 @@
 //     while(1);
 // /* USER CODE END */
 
-//     return 0;
-// }
+    return 0;
+}
 
 
-// /* USER CODE BEGIN (4) */
+/* USER CODE BEGIN (4) */
 
 // /*********************************************************************************
 //  *                          freeRTOS TASK IMPLEMENTATIONS
