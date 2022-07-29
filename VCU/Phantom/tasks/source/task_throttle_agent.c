b8b748c6b1851e81e14507bbaba20734b0b1282c
Guojiaxi <joshliguo@gmail.com>
2022-07-29 04:15:09 -0700

    completed base throttle agent, sim agent not implemented yet

--- VCU/Phantom/tasks/source/task_throttle_agent.c
+++ VCU/Phantom/tasks/source/task_throttle_agent.c
@@ -8,46 +8,87 @@
 #include "phantom_queue.h"
 // #include "phantom_timer.h"   // if you need to use timers
 
-// #include your_task_header_file
+#include "task_throttle_agent.h"
+#include "task_config.h"
 // Any other .h files you need goes here...
+#include "board_hardware.h"
+#include "adc.h"
 
 static Task task;
 static TaskHandle_t taskHandle; 
-static QueueHandle_t sendQueue;
+
+typedef struct _pedal_reading {
+    uint16_t bse;
+    uint16_t fp2;
+    uint16_t fp1;
+} pedal_reading_t;
 
 // Any other module-scope variables goes here... (make sure they have the 'static' keyword)
-// static TimerHandle_t yourTimerHandle;
-// static long yourTimerCounter;
+static QueueHandle_t throttleMailBox = NULL;
+static pedal_reading_t prevFilteredPedalRd = (pedal_reading_t) {0, 0, 0};
 
 // Pre-define your static functions here...
-static void vYourTaskFunction(void* arg);
-// static void yourTimerCallbackFunc(TimerHandle_t timer);
+static void vThrottleAgentTask(void* arg);
+static void vThrottleAgentSimTask(void* arg);
 
-void Task_throttleAgentSetSendQueue(QueueHandle_t queue_handle)
+QueueHandle_t ThrottleAgent_getMailBoxHandle()
 {
-    sendQueue = queue_handle;
+    return throttleMailBox;
 }
 
 void Task_throttleAgentInit(void)
 {
-    // task = (Task) {vYourTaskFunction, YOUR_TASK_PERIOD_MS};
+    task = (Task) {vThrottleAgentTask, THROTTLE_AGT_PERIOD_MS};
 
     // Phantom_createTask should block infinitely if task creation failed
-    // taskHandle = Phantom_createTask(&task, TASK_NAME, YOUR_TASK_STACK_SIZE, YOUR_TASK_PRIORITY);
-
-    // create your persistent timers here...
-    // yourTimerHandle = Phantom_createTimer(YOUR_TIMER_NAME, YOUR_TIMER_PERIOD_MS, AUTO_RELOAD, &yourTimerCounter, yourTimerCallbackFunc);
-    // Phantom_startTimer(yourTimerHandle);
+    taskHandle = Phantom_createTask(&task, "ThrottleAgentTask", THROTTLE_AGT_STACK_SIZE, THROTTLE_AGT_PRIORITY);
 
     // any other init code you want to put goes here...
+    throttleMailBox = Phantom_createMailBox(sizeof(pedal_reading_t));
 }
 
-static void vthrottleAgentTask(void* arg)
+static void vThrottleAgentTask(void* arg)
 {
     // arg will always be NULL, so ignore it.
 
-    // your task stuff goes in here...
+    // Get pedal readings from ADC
+    adcData_t FP_data[3];
+    adcStartConversion(adcREG1, adcGROUP1);
+    while (!adcIsConversionComplete(adcREG1, adcGROUP1)); // prefer another method of waiting, maybe an interrupt with TaskSuspend/Resume/YIELD?
+    adcGetData(adcREG1, adcGROUP1, FP_data);
+
+    // must map each value explicitly because compiler may not have the same mem packing rules for struct as arrays
+    pedal_reading_t pedalRd = (pedal_reading_t) {FP_data[0].value, FP_data[1].value, FP_data[2].value};
+
+    // apply a low pass filter with ALPHA of 0.5
+    pedalRd.bse = (pedalRd.bse + prevFilteredPedalRd.bse) >> 1;
+    pedalRd.fp1 = (pedalRd.fp1 + prevFilteredPedalRd.fp1) >> 1;
+    pedalRd.fp2 = (pedalRd.fp2 + prevFilteredPedalRd.fp2) >> 1;
+
+    // update prev filtered values
+    prevFilteredPedalRd = pedalRd;
+
+    // send filtered values to mailbox
+    Phantom_overwrite(throttleMailBox, &pedalRd);
+}
+
+
+/* SIMULATION MODE VARIANT */
+
+void Task_throttleAgentSimInit(void)
+{
+    task = (Task) {vThrottleAgentSimTask, THROTTLE_AGT_SIM_PERIOD_MS};
+
+    // Phantom_createTask should block infinitely if task creation failed
+    taskHandle = Phantom_createTask(&task, "ThrottleAgentSimTask", THROTTLE_AGT_SIM_STACK_SIZE, THROTTLE_AGT_SIM_PRIORITY);
+
+    // any other init code you want to put goes here...
+    throttleMailBox = Phantom_createMailBox(sizeof(pedal_reading_t));
+}
+
+static void vThrottleAgentSimTask(void* arg)
+{
+
 }
 
 // Other helper functions and callbacks goes here...
-// static void yourTimerCallbackFunc(TimerHandle_t timer) {}
