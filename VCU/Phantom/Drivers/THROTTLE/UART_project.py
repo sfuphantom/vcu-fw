19c5e092c2d754bfc4a35325f1dba34561ce8848
Kevin Litvin <89829682+kevinl03@users.noreply.github.com>
2022-09-30 13:19:23 -0700

    Uploadeded correct version

--- VCU/Phantom/Drivers/THROTTLE/UART_project.py
+++ VCU/Phantom/Drivers/THROTTLE/UART_project.py
@@ -1,9 +1,10 @@
 
+#Kevinl03
 
-# Import the argparse library
-import time
 import serial
+import serial.tools.list_ports
 import argparse
+import operator
 
 import os
 import sys
@@ -14,6 +15,39 @@ import sys
 #is done through built-in functions (check task_throttle).
 #------------------------     --------------------------------------
 
+
+#Following functions
+def get_available_serial_ports():
+    available_ports_all = list(serial.tools.list_ports.comports())               # get all available serial ports
+    available_ports = [port for port in available_ports_all if port[2] != 'n/a'] # remove all unfit serial ports
+    available_ports.sort(key=operator.itemgetter(1))                             # sort the list based on the port
+    return available_ports
+
+def select_a_serial_port(available_ports):                                       # TODO: check file_cfg for preselected serial port
+    global selected_port
+    if len(available_ports) == 0:       # list is empty -> exit
+        print("[!] No suitable serial port found.")
+        exit(-1)
+    elif len(available_ports) == 1:     # only one port available
+        (selected_port,_,_) = available_ports[0]
+        print("[+] Using only available serial port: %s" % selected_port)
+    else:                               # let user choose a port
+        successful_selection = False
+        while not successful_selection:
+            print("[+] Select one of the available serial ports:")
+            # port selection
+            item=1
+            for port,desc,_ in available_ports:
+                print ("    (%d) %s \"%s\"" % (item,port,desc))
+                item=item+1
+            selected_item = int(input(">>> "))                               # TODO: handle character input
+            # check if a valid item was selected
+            if (selected_item > 0) and (selected_item <= len(available_ports)):
+                (selected_port,_,_) = available_ports[selected_item-1]
+                successful_selection = True
+            else:
+                print("[!] Invalid serial port.\n")
+
 # Create the parser
 my_parser = argparse.ArgumentParser(description='Throttle Value Simulator')
 
@@ -21,41 +55,42 @@ my_parser = argparse.ArgumentParser(description='Throttle Value Simulator')
 # Add the arguments
 my_parser.add_argument('BSE_val',
                        metavar='BSE',
-                       type=float,
-                       help='Break Sensor : float')
+                       type=int,
+                       help='Break Sensor :     0 <= int <= 1000')
 
 my_parser.add_argument('APPS1_val',
                        metavar='APPS1',
-                       type=float,
-                       help='Accelerator Pedal 1 : float  ')
+                       type=int,
+                       help='Accelerator Pedal 1 : 0 <= int <= 1000  ')
                        
 
 my_parser.add_argument('APPS2_val',
                        metavar='APPS2',
-                       type=float,
-                       help='Accelerator Pedal 2 : float ')                      
+                       type=int,
+                       help='Accelerator Pedal 2 : 0 <= int <= 1000 ')                      
 
 # Execute the parse_args() method
 #Stores arguments in a Namespacc
 args = my_parser.parse_args()
 
+#get the ports and set the ports
+select_a_serial_port(get_available_serial_ports())
 
-ser = serial.Serial('COM7',9600)
+
+ser = serial.Serial(selected_port,9600)
 count = 0
 
 
-print("HELLO?")
+#create a list 
 mylist = []
 mylist.append(args.BSE_val)
 mylist.append(args.APPS1_val)
 mylist.append(args.APPS2_val)
 
-print(mylist)
 
-'''for i in range(0,3):
-    mylist[i] = str(mylist[i])
-    ser.write(mylist[i]).encode()
-    data = ser.readline()
-    print(data)
-'''
+
+# Write text or bytes to the file
+for word in mylist:
+    ser.write(word)
+    ser.write(",".encode('utf-8'))
 
