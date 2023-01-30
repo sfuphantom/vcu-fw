4ef5ae7e1d02b040962375408a862d687a5e32db
Kevin Litvin <ktl13@sfu.ca>
2023-01-30 10:38:16 -0800

    added comments for wave functionalities

--- command-line/RefactoringWaveForm.py
+++ command-line/RefactoringWaveForm.py
@@ -1,35 +1,32 @@
-import os;
+#Author: kevinl03 
+
 import random;
-import argparse
+import argparse 
 import math
-#import Throttle_Value_Simulator
+import Throttle_Value_Simulator
+
 
 import csv
 
 
 
-#-------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
 #THESE VARIABLES CAN BE CHANGED DEPENDING ON PEDAL/VCU SPECIFICATIONS
-#Can be found here : https://docs.google.com/document/d/1PsZSJR6la_u_oXmN2lo8tfRcU6E8GrMdpH35ESl7d4U/edit
-
-#ranges for petals
-
-maxPercentPressed = 50
-minPercentPressed = 0
-
-maxVoltageReading = 5
-minVoltageReading = 0
+#Most up to date specifications found here :
+# https://docs.google.com/document/d/1PsZSJR6la_u_oXmN2lo8tfRcU6E8GrMdpH35ESl7d4U/
 
 #ranges for VCU voltage
 APPS1maxVoltageReading = 4.5
 APPS1minVoltageReading = 1.50
 
-APPS2maxVoltageReading = 1.49
+APPS2maxVoltageReading = 1.5
 APPS2minVoltageReading = 0
 
 
 BSEmaxVoltageReading = 4.5
 BSEminVoltageReading = 1.50
+
+#Following 4 values are not used
 
 APPSMaxPedalAngle = 225
 APPSMinPedalAngle = 180
@@ -39,11 +36,11 @@ BSEMinPedalAngle = -74
 
 
 
-#-------------------------------------------------------------------#
+#--------------------------------------------------------------------------------#
 
 
 
-
+#Simple linear mapping, may need to refacotr if the Power Team's design is non-linear
 def mapPercentageToAPPS1Voltage(percentPressed):
     return percentPressed*(APPS1maxVoltageReading - APPS1minVoltageReading) + APPS1minVoltageReading
 
@@ -54,13 +51,16 @@ def mapPercentageToBSEVoltage(percentPressed):
     return percentPressed*(BSEmaxVoltageReading - BSEminVoltageReading)+ BSEminVoltageReading
 
 
+#Sin wave used for to simulate a car going around a turn where
+#The applied press is different when exiting a turn 
 def percentToSinWave(percent):
-    
-    VerticalScale = percent
 
     HorizontalScale = math.pi
 
     return (math.sin(percent*HorizontalScale))
+
+#Takes the value of percent pressed and reflected over the line y = 0.5
+#Such that the percent is 1-(sinwave(percentpressed)
 
 def percentToInverseSinWave(percent):
 
@@ -69,6 +69,9 @@ def percentToInverseSinWave(percent):
     return (-math.sin(percent*HorizontalScale)+1)
 
 
+
+
+#Generates the values for the Sinwave
 def sinWave(PedalType,PercentPressed, Values):
     
 
@@ -85,13 +88,12 @@ def sinWave(PedalType,PercentPressed, Values):
             Values[2] = mapPercentageToBSEVoltage(SinPercentage)
 
 def InversesinWave(PedalType,PercentPressed, Values):
-    
-
 
     SinPercentage = percentToInverseSinWave(PercentPressed)
     Values[2] = mapPercentageToBSEVoltage(SinPercentage)
 
 
+#Simulated Cons
 def triangularWave(PedalType,PercentPressed, Values):
     
     match PedalType:
@@ -105,9 +107,11 @@ def triangularWave(PedalType,PercentPressed, Values):
             Values[2] = mapPercentageToBSEVoltage(PercentPressed)
 
 
-def scatteredWave(PedalType,PercentPressed, Values):
 
-    randomPercentPressed = random.int(0,int(PercentPressed*100))
+#Generate random values to test firmware when voltage readings are discontinuous
+
+    randomPercentPressed = random.randint(0,int(PercentPressed*100))
+    randomPercentPressed = float(randomPercentPressed/100)
     match PedalType:
         case "APPS1":
             Values[0] = mapPercentageToAPPS1Voltage(randomPercentPressed)
@@ -117,38 +121,43 @@ def scatteredWave(PedalType,PercentPressed, Values):
 
         case "BSE":
             Values[2] = mapPercentageToBSEVoltage(randomPercentPressed)
+            print(mapPercentageToBSEVoltage(randomPercentPressed))
         
 
 
 
-    
+#Used for choosing which wave to generate     
 def matchInverse(APPSWaveForm,curpercentage,Values):
     match APPSWaveForm:
 
-                        case "S":
-                            InversesinWave("BSE",curpercentage, Values)
+        case "S":
+            InversesinWave("BSE",curpercentage, Values)
                         
-                        case "T":
-                            #Passing 1-curpercentage will give the percentage as a float which is the opposite of
-                            #the other reading
-                            triangularWave("BSE",1-curpercentage, Values)                        
-                        case "R":
-                            scatteredWave("BSE", 1-curpercentage, Values)
+        case "T":
+             #Passing 1-curpercentage will give the percentage as a float which is the opposite of
+             #the other reading
+            triangularWave("BSE",1-curpercentage, Values)                        
+        case "R":
+            scatteredWave("BSE", 1-curpercentage, Values)
         
-                        case "P":
-                            pass
+        case "P":
+            pass
                             
 
-                        case "O":
-                           Values[2] = BSEmaxVoltageReading
-                        case _:
-                            pass
+        case "O":
+            Values[2] = BSEmaxVoltageReading
+        case _:
+            pass
     
 
 if __name__ == "__main__":
 
+
     my_parser = argparse.ArgumentParser(description='Arbitrary Wave Form Generator')
 
+    
+    #Create arguments when calling the script
+    
     my_parser.add_argument('Cycles',
                            metavar='Cyles',
                            type=int,
@@ -171,6 +180,7 @@ if __name__ == "__main__":
                            help='T = triangular \n S = sinusodial \n R = Random \n I = Inverse of APPS \n O = Brake Values set to 0 ')
 
 
+    #read values from the commandline/terminal
     args = my_parser.parse_args()
 
     
@@ -184,17 +194,22 @@ if __name__ == "__main__":
         BSEWaveForm= args.BSEWaveForm
         APPSWaveForm = args.APPSWaveForm
 
-        #placeholder for set of values which will be filled in 
+        #placeholder for values [APPS1, APPS2, BSE]
+        #Will be changed based on the arguments and the current percentage pressed
 
         Values = [0,0,0]
 
         for cycle in range(numcycles):
 
 
-            for increment in range(0,precision+1):
+            
+            #Total number of values to represent the input wave
+            
+            for increment in range(0,precision):
+                #NOTE: percentage always represented as a number [0,1] not [0,100]
                 curpercentage = (increment/precision)
                 
-            
+                
                  
                 match APPSWaveForm:
                     case "S":
@@ -219,9 +234,10 @@ if __name__ == "__main__":
                         Values[1] = APPS2minVoltageReading
 
                     case _:
-                        print(APPSWaveForm, "Not an option")
+                        print("APPS waveform" , APPSWaveForm, "is not an option")
+                        exit()
                         
-
+                #Changes [ , , BSE] 
                 match BSEWaveForm:
                     case "S":
                         sinWave("BSE",curpercentage, Values)
@@ -234,15 +250,15 @@ if __name__ == "__main__":
                         scatteredWave("BSE",curpercentage, Values)
 
                     case "I":     
-                        matchInverse("BSE",curpercentage,Values)
+                        matchInverse(APPSWaveForm,curpercentage,Values)
                             
                     case "O":
                         Values[2] = BSEminVoltageReading
                     case _:
-                        pass
+                        print("BSE waveform" , APPSWaveForm, "is not an option")
+                        exit()
                            
-                           
-                        
+                #Writes the set of values to the CSV file           
                 csv.writer(f).writerow(Values)           
                 
 
@@ -251,4 +267,4 @@ if __name__ == "__main__":
         #APPS wave forms must allow to be tested independantly from BSE
 
     #Defined in ThrottleValue Simulator 
-    #Throttle_Value_Simulator.sendValsFromFile('csv_file.csv') #uncomment this line to send values to VCU
+    Throttle_Value_Simulator.sendValsFromFile('csv_file.csv') #uncomment this line to send values to VCU
