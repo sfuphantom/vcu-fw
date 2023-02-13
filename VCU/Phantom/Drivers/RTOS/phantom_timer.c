5d8c316e194c84ff1d31467f7f5577bae156a043
rafguevara <rafguevara14@gmail.com>
2023-02-13 12:52:09 -0800

    add timer error checking; rename parameters

--- VCU/Phantom/Drivers/RTOS/phantom_timer.c
+++ VCU/Phantom/Drivers/RTOS/phantom_timer.c
@@ -4,14 +4,11 @@
  *  Created on: Aug. 25, 2021
  *      Author: Josh Guo
  */
-#include "FreeRTOS.h"
-#include "FreeRTOSConfig.h"
 
-// this needs to be here because os_timer.h does not include this despite trying to reference it
-typedef void *TaskHandle_t; 
-#include "os_timer.h"
 
 #include "phantom_timer.h"
+#include "task_logger.h"
+#include "state_machine.h"
 
 // #define DEBUG    // uncomment this line for ASSERT statements to work
 
@@ -23,13 +20,13 @@ static int numOfTimers = 0;
     timerName:          Just a text name, not used by the RTOS kernel.
     periodMS:           The timer period in ticks, must be greater than 0.
     isAutoReloading:    If true, the timer will auto-reload itself when they expire.
-    counterPtr:         Pointer to a number which increments whenever the timer expires, initialized to 0.
+    timerID:         Pointer to a number which increments whenever the timer expires, initialized to 0.
     callbackFunction:   Callback function that is called when the timer expires.
 */
 TimerHandle_t Phantom_createTimer(char* const timerName, 
                                   uint32 periodMS, 
                                   bool isAutoReloading, 
-                                  void* counterPtr, 
+                                  TimerID_t timerID,
                                   TimerCallbackFunction_t callbackFunction)
 {
     if (numOfTimers >= MAX_NUMBER_OF_TIMERS) {
@@ -40,7 +37,7 @@ TimerHandle_t Phantom_createTimer(char* const timerName,
         timerName,
         pdMS_TO_TICKS(periodMS),
         isAutoReloading,
-        counterPtr,
+        (void*)timerID,
         callbackFunction
     );
 
@@ -48,14 +45,24 @@ TimerHandle_t Phantom_createTimer(char* const timerName,
     return newTimer;
 }
 
-uint8 Phantom_startTimer(TimerHandle_t timer, uint32 waitTimeMs)
+static uint8 LogTimerInfo(TimerHandle_t timer, char* str)
 {
-    return xTimerStart(timer, pdMS_TO_TICKS(waitTimeMs));
+    char buffer[32];
+    snprintf(buffer, 32, "(%s) %s", pcTimerGetName(timer), str);
+
+    return Log(buffer);
 }
 
 uint8 Phantom_stopTimer(TimerHandle_t timer, uint32 waitTimeMs)
 {
-    return xTimerStop(timer, pdMS_TO_TICKS(waitTimeMs));
+    uint8_t ret = xTimerStop(timer, pdMS_TO_TICKS(waitTimeMs)) == pdPASS;
+
+    if (!ret)
+    {
+        LogTimerInfo(timer, "Failed to stop timer");
+    }
+    
+    return ret;
 }
 
 uint8 Phantom_deleteTimer(TimerHandle_t timer, uint32 waitTimeMs)
@@ -72,5 +79,26 @@ int Phantom_getNumberOfTimers(void)
 
 bool Phantom_isTimerActive(TimerHandle_t timer)
 {
-    return xTimerIsTimerActive(timer);
+    uint8_t ret = xTimerIsTimerActive(timer) == pdPASS;
+    
+    return ret;
+}
+
+uint8 Phantom_startTimer(TimerHandle_t timer, uint32 waitTimeMs)
+{
+    uint8_t started = true;
+
+    if (!Phantom_isTimerActive(timer))
+    {
+        started = xTimerStart(timer, pdMS_TO_TICKS(waitTimeMs)) != pdFALSE;
+    }
+
+
+    if (!started)
+    {
+        LogTimerInfo(timer, "Timer not started");
+    }
+
+    return started;
+    
 }
