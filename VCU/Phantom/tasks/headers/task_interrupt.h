12f4fb85c10b4241c3b47b9807ca96c83026bce9
rafguevara <rafguevara14@gmail.com>
2023-02-13 12:51:12 -0800

    cleanup task and driver files

--- VCU/Phantom/tasks/headers/task_interrupt.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHANTOM_TASKS_INTERRUPT_ACTOR_H_
-#define PHANTOM_TASKS_INTERRUPT_ACTOR_H_
-
-#include <stdint.h>
-
-void InterruptInit(void);
-uint8_t receiveIntEvent(uint32_t delay);
-
-#endif
