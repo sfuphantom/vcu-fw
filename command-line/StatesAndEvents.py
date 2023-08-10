d6383f74369d7cddbeff38280c6304fac9040a04
Kevin Litvin <klitvin101@gmail.com>
2023-08-09 20:06:12 -0700

    feat: events now stored in a set to improve checking duplicate runtime and update vcu event documentation

--- command-line/StatesAndEvents.py
+++ command-line/StatesAndEvents.py
@@ -9,6 +9,10 @@ class VCUEvents (Enum):
     """
     The VCU Events, refer to VCU\Phantom\data_structures\vcu_common.h
     """
+
+    # BEGIN_OF_EVENTS=0, Corresponding enum from the above referenced header file.
+    #                    NOTE: Unimplemented here because it is not a valid event
+    #                          that could (or should) be raised by the VCU   
     EVENT_APPS1_RANGE_FAULT = 1
     EVENT_APPS2_RANGE_FAULT = 2
     EVENT_BSE_RANGE_FAULT = 3
@@ -20,6 +24,10 @@ class VCUEvents (Enum):
     EVENT_BRAKE_PLAUSIBILITY_CLEARED = 9
     EVENT_BRAKE_PLAUSIBILITY_FAULT = 10
     EVENT_UNRESPONSIVE_APPS = 11
+
+    # END_OF_EVENTS again, implemeted in the header file, 
+    #                      but we do not implement it as it is not a valid event
+
 class VCUStates(Enum):
     """
     The VCU states, refer to VCU\Phantom\data_structures\vcu_common.h
@@ -35,6 +43,9 @@ class EventData:
     EVENT: VCUEvents
     TIME: int
 
+    def __hash__(self):
+        return hash((self.EVENT,self.TIME))
+
 @dataclass
 class StateData:
     STATE: VCUStates
@@ -48,20 +59,20 @@ class ResponseVCU:
     Allows for the same events to be stored if triggered at different times
     It is necessary for the this class to specify the VCU state
     
-    Stores an array of events and their time of trigger
+    Stores the set of events and their time of trigger
     Stores the state of the VCU and the time of change (if changed)
     """
 
     def __init__(self):
-        self._events: List[EventData] = []
+        self._events: set[EventData] = set()
         self._state : StateData = None
     
     def add_event(self, event_name: VCUEvents, event_time: int):
         """
-        Add an event to the list of events
+        Add an event to set of events
 
         :param event_name: a VCU event
-        :param event_time: the relative time of the event
+        :param event_time: the relative time of the event trigger
         """
 
         #check that the event exists in the available VCU Events enum
@@ -70,10 +81,10 @@ class ResponseVCU:
         
         event_data = EventData(EVENT=event_name,TIME=event_time)
         
-        if not self._check_duplicate(event_data):
+        if event_data in self._events:
             raise EventError(f"Duplicate event : {event_name} at time {event_time}")
     
-        self._events.append(event_data)
+        self._events.add(event_data)
 
 
     def set_state(self, state: VCUStates, state_time_trigger: int = None):
@@ -88,19 +99,6 @@ class ResponseVCU:
         
         self._state = StateData(STATE=state, TIME=state_time_trigger)
 
-    def _check_duplicate(self, event_data: EventData) -> bool:
-        """
-        Check that there does not already exist an event
-        with the same time and type
-
-        :param event_data: the event data to compare to the existing events
-        :return: returns true if no duplicate events are given
-        """
-        for existing_event in self.events:
-            if existing_event.EVENT == event_data.EVENT \
-               and existing_event.TIME == event_data.TIME:
-                  return False
-        return True
 
     @property
     def events(self) -> List[EventData]:
@@ -118,4 +116,3 @@ class StateError(Exception):
     """
     Exceptions for VCU states 
     """
-
