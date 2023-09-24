993c2adfcd2a44d0aed8600c5ba254c2d47e5aed
Kevin Litvin <klitvin101@gmail.com>
2023-09-23 18:13:53 -0700

    Added script to communicate with google cloud, json file for cloud access will be distributed privately to avoid uploading to our public Team Phantom repo

--- command-line/UploadDrive.py
+++ command-line/UploadDrive.py
@@ -1,57 +1,100 @@
-import requests
-import json
+import os
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
 
-headers = {"Authorization": "Bearer ya29.a0AfB_byAl4p4lHJVn90vyMudsSneTlEqVp6dgz_M0grLRVGUe3nG07R7XLdBoDeeoZUOrLijvoPgY-yNGQZmGV6WskeEGt8JcwPUGipSTBpCdkH3TZXFoQwzgXev2f9TspYSpekBjm1ViQ_w9YMYZmaziTHdLmbwx69r0aCgYKAUQSARISFQGOcNnClEeC-FM7O9m-lvHPz_W9WA0171"}
-para = {
-    "name": "SimVals.csv"
-}
 
-files = {
-    'data' : ('metadata', json.dumps(para), 'application/json; charset=utf-8'),
-    'file' : open("SimulatedValues.csv", "rb")
-}
+from datetime import datetime
+from pathlib import Path
 
-r = requests.post(
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-    headers=headers,
-    files=files
-)
 
-print(r)
+# Replace with the path to your credentials JSON file
+credentials_file = 'team-phantom-drive-writting-2d92b03341f0.json'
 
-# import os
-# from googleapiclient.discovery import build
-# from google.oauth2 import service_account
+# Define the folder ID of the Google Drive folder where you want to upload the text file
+folder_id = 'Myfolder'
 
-# # Replace with the path to your credentials JSON file
-# credentials_file = 'credentials.json'
 
-# # Define the folder ID of the Google Drive folder where you want to upload the image
-# folder_id = 'TestWriteFolder'
+def share_file_with_user(service, file_id, user_email, role='reader'):
+    permission = {
+        'type': 'user',
+        'role': role,
+        'emailAddress': user_email
+    }
+    service.permissions().create(
+        fileId=file_id,
+        body=permission,
+        sendNotificationEmail=False
+    ).execute()
 
-# def authenticate_gdrive():
-#     import pdb; pdb.set_trace()
-#     creds = service_account.Credentials.from_service_account_file(credentials_file, scopes=['https://www.googleapis.com/auth/drive'])
-#     service = build('drive', 'v3', credentials=creds)
-#     return service
 
-# def upload_image_to_gdrive(service, file_path, folder_id):
-#     file_name = os.path.basename(file_path)
-#     file_metadata = {'name': file_name, 'parents': [folder_id]}
+def authenticate_gdrive():
+    creds = service_account.Credentials.from_service_account_file(credentials_file, scopes=['https://www.googleapis.com/auth/drive'])
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+def upload_csv_to_gdrive(service, file_path, folder_id):
+
+    #debugging
+    #import pdb; pdb.set_trace()
+    file_name = Path(file_path).stem
+    file_ext = Path(file_path).suffix
+    now = datetime.now()
+    date_time = now.strftime("%Y-%m-%d-%H-%M-%S-")
     
-#     media = service.files().create(
-#         body=file_metadata,
-#         media_body=file_path,
-#         supportsAllDrives=True
-#     ).execute()
-    
-#     print(f'Uploaded image: {file_name} (File ID: {media["id"]})')
+    #write the files as date first so they can be sorted
+    file_metadata = {
+        'name': date_time + file_name + file_ext,
+        }
 
-# if __name__ == '__main__':
-#     service = authenticate_gdrive()
-    
-#     # Replace 'image_to_upload.jpg' with the path to the image you want to upload
-#     image_to_upload = 'image_to_upload.jpg'
-    
-#     upload_image_to_gdrive(service, image_to_upload, folder_id)
+    media = MediaFileUpload(file_path, mimetype='text/csv')
 
+    
+    media = service.files().create(
+        body=file_metadata,
+        media_body=media,
+        supportsAllDrives=True
+    ).execute()
+    
+    #print(f'Uploaded CSV file: {file_name} (File ID: {media["id"]})')
+
+    #share with the logged in user/team phantom drive.
+    user_email = 'klitvin101@gmail.com'  # Replace with your personal Google account email
+    share_file_with_user(service, media["id"], user_email)
+
+    
+
+if __name__ == '__main__':
+    service = authenticate_gdrive()
+    
+    # Replace 'data.csv' with the path to the CSV file you want to upload in the relative directory
+    csv_file_to_upload = "SimulatedValues.csv" # current date and time for file uniqueness
+    
+    upload_csv_to_gdrive(service, csv_file_to_upload, folder_id)
+
+
+
+#--------------Unused---------------#
+#->access tokens refresh every 3600 seconds, and we would need to write a script to query 
+#when a tokens get's refreshed
+# import requests
+# import json
+
+# headers = {"Authorization": "Bearer ya29.a0AfB_byAl4p4lHJVn90vyMudsSneTlEqVp6dgz_M0grLRVGUe3nG07R7XLdBoDeeoZUOrLijvoPgY-yNGQZmGV6WskeEGt8JcwPUGipSTBpCdkH3TZXFoQwzgXev2f9TspYSpekBjm1ViQ_w9YMYZmaziTHdLmbwx69r0aCgYKAUQSARISFQGOcNnClEeC-FM7O9m-lvHPz_W9WA0171"}
+# para = {
+#     "name": "TestVals/SimValsLOL.csv",
+#     "parents" : "Myfolder/"
+# }
+
+# files = {
+#     'data' : ('metadata', json.dumps(para), 'application/json; charset=utf-8'),
+#     'file' : open("SimulatedValues.csv", "rb")
+# }
+
+# r = requests.post(
+#     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+#     headers=headers,
+#     files=files
+# )
+
+# print(r)
