c607198519a98c3f3c18fef23e4d353bf73acd05
kevinl03 <klitvin101@gmail.com>
2023-10-14 12:21:15 -0700

    Added google drive interface specifically for VCU testing

--- command-line/UploadDrive.py
+++ command-line/UploadDrive.py
@@ -15,11 +15,6 @@ from generate_simulation import CSV_FILE_NAME as CSV_FILE_NAME
 from PointPlotting import PLOT_IMAGE_NAME as PLOT_IMAGE_NAME
 
 
-# Replace with the path to your credentials JSON file
-CREDENTIAL_FILE = 'team-phantom-drive-writting-2d92b03341f0.json'
-
-# Define the folder ID of the Google Drive folder where you want to upload the text file
-VCU_SIM_LOG_FOLDER_ID = '1wn_EWejA6HYhonjLiEfYkdMhRBiJK7p7'
 
 class GoogleDriveAuthenticator:
 
@@ -43,7 +38,7 @@ class GoogleDriveAuthenticator:
 
 class GooleDriveUpload:
 
-    def __init__(self, drive_service : build , folder_id : str = VCU_SIM_LOG_FOLDER_ID):
+    def __init__(self, drive_service : build , folder_id : str):
         self.drive_service : Resource= drive_service
         self.folder_id: str = folder_id
     
@@ -87,7 +82,7 @@ class GooleDriveUpload:
         file_metadata = {
             'name': csv_file_name,
             'parents': [parent_folder_id]
-            }
+         }
 
         media = MediaFileUpload(csv_file_path, mimetype='text/csv')
 
@@ -97,16 +92,20 @@ class GooleDriveUpload:
             supportsAllDrives=True
         ).execute()
 
-        # Set the file as read-only (Viewer)
-        self.drive_service.permissions().create(
-            fileId=media['id'],
-            body={'role': 'reader', 'type': 'anyone'}
-        ).execute()
+        self.set_read_only_permissions(file_id = media['id'])
 
         print(f'Uploaded CSV file: {csv_file_name} (File ID: {media["id"]})')
 
         return True
+    
+    def set_read_only_permissions(self, file_id: str):
+         # Set the file as read-only (Viewer)
+        self.drive_service.permissions().create(
+            fileId=file_id,
+            body={'role': 'reader', 'type': 'anyone'}
+        ).execute()
 
+        return True
 
     def upload_png_to_gdrive(self, image_file_path: str, parent_folder_id: str) -> bool:
 
@@ -140,25 +139,48 @@ class GooleDriveUpload:
         file_stem = Path(file_path).stem
         file_ext = Path(file_path).suffix
         return file_stem + file_ext
+    
+class VcuGDriveInterface:
+    """
+    Specific class to interact with the VCU logging folder within Team Phantom Google Drive
+    """
 
-if __name__ == '__main__':
-    Authenticator = GoogleDriveAuthenticator(credentials_path=CREDENTIAL_FILE)
-    credentials = Authenticator.generate_credentials()
-    gdrive_service = Authenticator.authenticate_gdrive(credentials=credentials)
+    #DO NOT POST THIS ON THE GITHUB OPEN SOURCE REPO
+    CREDENTIAL_FILE = 'team-phantom-drive-writting-2d92b03341f0.json'
 
     #parent folder is the folder Id for the following team drive directory: 
-    """
-    Drive/Phantom Engineering/Controls/2023/Test Logs-Simulation/VCU
-    """
 
-    UploadGDrive = GooleDriveUpload(gdrive_service)
+    #---Drive/Phantom Engineering/Controls/2023/Test Logs-Simulation/VCU---
+    VCU_SIM_LOG_FOLDER_ID = '1wn_EWejA6HYhonjLiEfYkdMhRBiJK7p7'
 
-    now = datetime.now()
-    date_time = now.strftime("%Y-%m-%d-%H-%M-%S-")
+    def __init__(self) -> None:
+        Authenticator = GoogleDriveAuthenticator(self.CREDENTIAL_FILE)
+        credentials = Authenticator.generate_credentials()
+        gdrive_service = Authenticator.authenticate_gdrive(credentials=credentials)
+        self.upload_gdrive = GooleDriveUpload(gdrive_service, self.VCU_SIM_LOG_FOLDER_ID)
+
+    def upload_data(self, csv_file_name: str = CSV_FILE_NAME, plotted_data:str = PLOT_IMAGE_NAME):
+        #write the files as date first so they can be sorted
+        now = datetime.now()
+        date_time = now.strftime("%Y-%m-%d-%H-%M-%S-") 
+        folder_name = date_time + "Sim"
+
+        #create the parent folder to store the CSV and data
+        try: 
+            parentID: str = self.upload_gdrive.create_parent_folder(
+                                                folder_name=folder_name, 
+                                                parent_folder_id=self.VCU_SIM_LOG_FOLDER_ID)
+            #upload the csv
+            self.upload_gdrive.upload_csv_to_gdrive(
+                                CSV_FILE_NAME, parentID)
+            #upload the png
+            self.upload_gdrive.upload_png_to_gdrive(
+                                PLOT_IMAGE_NAME, parentID)
+        except Exception as e:
+            print(f"Exception Caught: {e}")
+            return False
+        
+        return True
+
     
-    #write the files as date first so they can be sorted
-    folder_name = date_time + "Sim"
-    parentID: str = UploadGDrive.create_parent_folder(folder_name=folder_name, parent_folder_id=VCU_SIM_LOG_FOLDER_ID)
-    UploadGDrive.upload_csv_to_gdrive(CSV_FILE_NAME, parentID)
-    UploadGDrive.upload_png_to_gdrive(PLOT_IMAGE_NAME, parentID)
 
